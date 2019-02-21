24cf132e916fba6b9e6b18f3fe25c3120e890e22
LWZXS <newslwz@gmail.com>
2019-02-21 11:29:25 +0800

    test

--- app/test.py
+++ app/test.py
@@ -1,4 +1,10 @@
-def main():
-    print "sasasasas"
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
 if __name__ == '__main__':
-    main()
+    app.debug = True
+    app.run(host='0.0.0.0')
